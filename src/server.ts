59bcbd807ea39a77173fce5ad05d3041914ded52
abdulhakeem1232 <hakeemforgit@gmail.com>
2024-05-20 17:21:44 +0530

    first commit

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv'
+import path from 'path'
+import * as grpc from "@grpc/grpc-js";
+import * as protoLoader from "@grpc/proto-loader";
+import { UserController } from './controllers/jobController';
+import { connectDB } from './config/db';
+
+dotenv.config()
+connectDB()
+
+const packageDefinition = protoLoader.loadSync(path.join(__dirname, "proto/user.proto"))
+const userProto = grpc.loadPackageDefinition(packageDefinition) as any;
+
+const server = new grpc.Server();
+
+const grpcServer = () => {
+    server.bindAsync(
+        `0.0.0.0:${process.env.PORT}`,
+        grpc.ServerCredentials.createInsecure(),
+        (err, port) => {
+            if (err) {
+                console.log(err, "error happened grpc user service");
+                return;
+            }
+            console.log("grpc user server started on port:", port);
+        }
+    );
+};
+
+server.addService(userProto.UserServices.service, {
+
+})
+
+grpcServer();
